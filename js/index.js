b87141cf16f9c938e6123865de337d0f14664ea7
Tyler Moline <tylermoline95@gmail.com>
2022-04-13 13:49:46 -0600

    optimized for screen size 768px

--- js/index.js
+++ js/index.js
@@ -13,4 +13,11 @@
   });
 })();
 
-// Gallery on click event
+const navButton = document.querySelector("button[aria-expanded]");
+
+function toggleNav({ target }) {
+  const expanded = target.getAttribute("aria-expanded") === "true" || false;
+  navButton.setAttribute("aria-expanded", !expanded);
+}
+
+navButton.addEventListener("click", toggleNav);
