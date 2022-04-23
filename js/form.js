b30bfb8d65fad8cbae685c9fe20a758ab112257c
Tyler Moline <tylermoline95@gmail.com>
2022-04-23 12:17:14 -0600

    set up forms

--- js/form.js
+++ js/form.js
@@ -2,6 +2,7 @@
   const form = document.getElementById("footerForm");
   const nameInput = document.getElementById("footerName");
   const emailInput = document.getElementById("footerEmail");
+  const phoneInput = document.getElementById("footerPhoneNumber");
   const messageInput = document.getElementById("footerMessage");
 
   async function handleSubmit(e) {
@@ -10,14 +11,17 @@
     const name = nameInput.value;
     const email = emailInput.value;
     const message = messageInput.value;
+    const phone = phoneInput.value;
 
-    if (!name || !email || !message) return alert("Please fill out all fields");
+    if (!name || !email || !message || !phone)
+      return alert("Please fill out all fields");
 
     const formData = new FormData();
 
     formData.append("name", name);
     formData.append("email", email);
     formData.append("message", message);
+    formData.append("phone", phone);
 
     const response = await fetch("/", {
       method: "POST",
@@ -33,6 +37,5 @@
   }
 
   // TODO: uncomment this when ready to test form
-  // form.addEventListener("submit", handleSubmit);
-  form.addEventListener("submit", () => alert("success"));
+  form.addEventListener("submit", handleSubmit);
 })();
